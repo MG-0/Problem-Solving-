53afdbfcd53aa0c9e2c21f8915fd510dd2979b4c
Mahmoud Gamal <gamalgit@gmail.com>
2026-06-26 18:45:09 +0300

    Implement Kadane's Algorithm for maximum subarray problem

--- Arrays/Medium/maximumSubarray.js
+++ Arrays/Medium/maximumSubarray.js
@@ -1,1 +1,14 @@
 
+
+// Kadane's Algorithm
+
+function maxSubArray (nums) {
+    let currSum = nums[0];
+    let maxSum = nums[0];
+    for (let i = 0; i< nums.length; i++) {
+        currSum = Math.max(nums[i], currSum + nums[i])
+        maxSum = Math.max(maxSum, currSum)
+    }
+    return maxSum
+}
+console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4])); // big o => o(n)
